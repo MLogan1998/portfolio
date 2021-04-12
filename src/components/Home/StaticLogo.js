077a2e64ca9d7e3caa92b0a47d8a123743a05c32
Matt Logan <mlogan5212@gmail.com>
2021-04-11 21:12:48 -0500

    Add responsive styles. Update project descriptions.

--- src/components/Home/StaticLogo.js
+++ src/components/Home/StaticLogo.js
@@ -4,11 +4,11 @@ import React from 'react';
 export const StaticLogo = (props) => {
   return (
       <div className="logo_container">
-        <div className="logo_image noshow" style={{ border: `13px solid ${props.color}` }} ></div>
+        <div className="logo_image noshow notab" style={{ border: `13px solid ${props.color}` }} ></div>
         <div className="link_container">
-          <a href='https://www.linkedin.com/in/mlogan5212/' target="_blank" rel="noopener noreferrer"><i className='noshow fab fa-linkedin link_container--icon' style={{ color: props.color }}></i></a>
-          <a href='https://github.com/MLogan1998' target="_blank" rel="noopener noreferrer"><i className='noshow fab fa-github-square link_container--icon' style={{ color: props.color }}></i></a>
-          <a href='https://drive.google.com/file/d/1lb8KNlu7K3n_1ju8j3kBojv9nsqXzKx_/view?usp=sharing' target="_blank" rel="noopener noreferrer"><i className='noshow fas fa-file-pdf link_container--icon' style={{ color: props.color }}></i></a>
+          <a href='https://www.linkedin.com/in/mlogan5212/' target="_blank" rel="noopener noreferrer"><i className='noshow notab fab fa-linkedin link_container--icon' style={{ color: props.color }}></i></a>
+          <a href='https://github.com/MLogan1998' target="_blank" rel="noopener noreferrer"><i className='noshow notab fab fa-github-square link_container--icon' style={{ color: props.color }}></i></a>
+          <a href='https://drive.google.com/file/d/1lb8KNlu7K3n_1ju8j3kBojv9nsqXzKx_/view?usp=sharing' target="_blank" rel="noopener noreferrer"><i className='noshow notab fas fa-file-pdf link_container--icon' style={{ color: props.color }}></i></a>
         </div>
       </div>
   );
